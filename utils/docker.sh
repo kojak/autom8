04bf3d6d99056ebfa735b17a9766f20ad91ba6df
Salim Badakhchani <salim.badakhchani@bluebank.io>
2017-10-21 23:33:08 +0100

    removed ocp

--- utils/docker.sh
+++ utils/docker.sh
@@ -2,5 +2,5 @@
 # Make sure you have logged into the openshift console and get retrived your token
 set -x
 
-REG="https://registry.ocp.example.com"
+REG="https://registry.example.com"
 sudo docker login --username=$(oc whoami) --password=$(oc whoami -t) ${REG}
